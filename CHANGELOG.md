62c9993c4190381d6214f05fd526415220415032
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2025-05-09 08:37:36 +0700

    commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `bale-nawasara` will be documented in this file.
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Paparee\BaleNawasara\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
