a9030939130db4f11c22ed1bc5e8e2080dd359f4
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2025-05-10 16:51:15 +0700

    add missing class

--- src/BaleNawasaraServiceProvider.php
+++ src/BaleNawasaraServiceProvider.php
@@ -4,6 +4,7 @@ namespace Paparee\BaleNawasara;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schedule;
+use Paparee\BaleNawasara\Commands\BaleNawasaraCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
