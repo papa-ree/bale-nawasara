d78fe2a3cd28862e8424216d7fb04e26d403af6e
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2025-05-25 16:01:58 +0700

    add hearbeat feature

---
+++ src/App/Jobs/HeartBeat.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Paparee\BaleNawasara\App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Http;
+
+class HeartBeat implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        $response = Http::withBasicAuth(env('WHATSAPP_GO_USER'), env('WHATSAPP_GO_PASSWORD'))
+        ->withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ])
+        ->post(env('WHATSAPP_GO_URL').'/send/message', [
+            'phone' => '6285239146416@s.whatsapp.net',
+            'message' => "i'm Available!",
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+}
