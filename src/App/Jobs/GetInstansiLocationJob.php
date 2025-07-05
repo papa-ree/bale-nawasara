ef0a88d75efc3acbf6efe6d803aacf935cefc92a
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2025-07-05 19:47:07 +0700

    Update GetInstansiLocationJob.php

--- src/App/Jobs/GetInstansiLocationJob.php
+++ src/App/Jobs/GetInstansiLocationJob.php
@@ -14,31 +14,25 @@ class GetInstansiLocationJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected string $url = 'https://sadap.ponorogo.go.id/api/dataPeta';
-
-    protected string $cacheKey = 'bale_inv_maps';
-
-    protected int $cacheMinutes = 1440;
-
     /**
      * Execute the job.
      */
     public function handle(): void
     {
-        Cache::remember($this->cacheKey, now()->addMinutes($this->cacheMinutes), function () {
-            $response = Http::get($this->url)->json();
+        $response = Http::get(config('bale-nawasara.instansi_location.api'))->json();
 
-            $data = $response['data'] ?? [];
+        $data = $response['data'] ?? [];
 
-            return collect($data)->map(function ($item) {
-                return [
-                    'id' => $item['id'] ?? '',
-                    'name' => $item['name'] ?? '',
-                    'description' => $item['description'] ?? '',
-                    'alamat' => $item['alamat'] ?? '',
-                    'map' => $item['map'] ?? '',
-                ];
-            });
+        $instansi = collect($data)->map(function ($item) {
+            return [
+                'id' => $item['id'] ?? '',
+                'name' => $item['name'] ?? '',
+                'description' => $item['description'] ?? '',
+                'alamat' => $item['alamat'] ?? '',
+                'map' => $item['map'] ?? '',
+            ];
         });
+
+        Cache::put('nawasara_instansi', $instansi);
     }
 }
