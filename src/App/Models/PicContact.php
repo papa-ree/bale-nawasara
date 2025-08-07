830d322544d85c859d5ab7059ce756e14e2abb68
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2025-08-07 09:43:06 +0700

    fix null recovery email pic

--- src/App/Models/PicContact.php
+++ src/App/Models/PicContact.php
@@ -18,78 +18,83 @@ class PicContact extends Model
     protected function contactName(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? Str::ucfirst($value) : null,
-            set: fn (?string $value) => $value ? Str::lower($value) : null,
+            get: fn(?string $value) => $value ? Str::ucfirst($value) : null,
+            set: fn(?string $value) => $value ? Str::lower($value) : null,
         );
     }
 
     protected function contactJob(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? Str::ucfirst($value) : null,
-            set: fn (?string $value) => $value ? Str::lower($value) : null,
+            get: fn(?string $value) => $value ? Str::ucfirst($value) : null,
+            set: fn(?string $value) => $value ? Str::lower($value) : null,
         );
     }
 
     protected function contactOffice(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? Str::ucfirst($value) : null,
-            set: fn (?string $value) => $value ? Str::lower($value) : null,
+            get: fn(?string $value) => $value ? Str::ucfirst($value) : null,
+            set: fn(?string $value) => $value ? Str::lower($value) : null,
         );
     }
 
     protected function contactPhone(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? Crypt::decryptString($value) : null,
-            set: fn (?string $value) => $value ? Crypt::encryptString($value) : null,
+            get: fn(?string $value) => $value ? Crypt::decryptString($value) : null,
+            set: fn(?string $value) => $value ? Crypt::encryptString($value) : null,
         );
     }
 
     protected function contactPhoneHash(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? hash('sha256', $value) : null,
-            set: fn (?string $value) => $value ? hash('sha256', $value) : null,
+            get: fn(?string $value) => $value ? hash('sha256', $value) : null,
+            set: fn(?string $value) => $value ? hash('sha256', $value) : null,
         );
     }
 
     protected function contactNip(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? Crypt::decryptString($value) : null,
-            set: fn (?string $value) => $value ? Crypt::encryptString($value) : null,
+            get: fn(?string $value) => $value ? Crypt::decryptString($value) : null,
+            set: fn(?string $value) => $value ? Crypt::encryptString($value) : null,
         );
     }
 
     protected function contactNipHash(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? hash('sha256', $value) : null,
-            set: fn (?string $value) => $value ? hash('sha256', $value) : null,
+            get: fn(?string $value) => $value ? hash('sha256', $value) : null,
+            set: fn(?string $value) => $value ? hash('sha256', $value) : null,
         );
     }
 
     protected function recoveryEmailAddress(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? Crypt::decryptString($value) : '-',
-            set: fn (?string $value) => $value ? Crypt::encryptString($value) : '-',
+            get: fn(?string $value) => $value ? Crypt::decryptString($value) : null,
+            set: fn(?string $value) => $value ? Crypt::encryptString($value) : null,
         );
     }
 
     protected function recoveryEmailAddressHash(): Attribute
     {
         return Attribute::make(
-            get: fn (?string $value) => $value ? hash('sha256', $value) : null,
-            set: fn (?string $value) => $value ? hash('sha256', $value) : null,
+            get: fn(?string $value) => $value ? hash('sha256', $value) : null,
+            set: fn(?string $value) => $value ? hash('sha256', $value) : null,
         );
     }
 
     public function subdomains(): HasMany
     {
         return $this->hasMany(DnsRecord::class);
+    }
+
+    public function ipPublics(): HasMany
+    {
+        return $this->hasMany(IpPublic::class);
     }
 
     public function email(): HasMany
